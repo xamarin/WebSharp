335ec4ba1c5f1c2ee56ec66b7f209fff86ca1f23
Kenneth Pouncey <kjpou@pt.lu>
2016-07-05 09:52:38 +0200

    Add GettingStarted project to go along with instructions

---
+++ GettingStarted/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using PepperSharp;
+
+namespace GettingStarted
+{
+    public class HelloWorld : Instance
+    {
+        public override bool Init(int argc, string[] argn, string[] argv)
+        {
+            LogToConsoleWithSource(PPLogLevel.Log, "GettingStarted.HelloWorld", "HelloWorld from C#");
+            return true;
+        }
+    }
+}
