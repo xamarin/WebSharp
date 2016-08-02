980c6abf452fecbbc19b1f2503d1381e65de019b
Kenneth Pouncey <kjpou@pt.lu>
2016-08-02 17:16:09 +0200

    Add support for 64 bit architectures to the register node.js module

--- Tools/electron-dotnet/lib/register.js
+++ Tools/electron-dotnet/lib/register.js
@@ -9,8 +9,12 @@ function Register(pluginPath) {
     const isWindows = process.platform === 'win32';
 
     var ppapiPath = '';
-    if (isWindows)
-        ppapiPath = __dirname + '\\..\\bin\\Win32\\PepperPlugin.dll';
+    if (isWindows) {
+        if (process.arch = 'x64')
+            ppapiPath = __dirname + '\\..\\bin\\x64\\PepperPlugin.dll';
+        else
+            ppapiPath = __dirname + '\\..\\bin\\Win32\\PepperPlugin.dll';
+    }
     if (pluginPath)
         ppapiPath = pluginPath + '\\PepperPlugin.dll';
     //console.log('PPAPI path ' + ppapiPath + ';application/electron-dotnet');
