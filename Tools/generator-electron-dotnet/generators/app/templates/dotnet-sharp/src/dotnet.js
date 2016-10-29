01e7bfc7cb6df41c2343a68624911416b3d045fc
Kenneth Pouncey <kjpou@pt.lu>
2016-10-29 12:19:14 +0200

    Update misspelling in templates

--- Tools/generator-electron-dotnet/generators/app/templates/dotnet-sharp/src/dotnet.js
+++ Tools/generator-electron-dotnet/generators/app/templates/dotnet-sharp/src/dotnet.js
@@ -2,7 +2,7 @@ var dotnet = require('electron-dotnet');
 
 var hello = dotnet.func('async (input) => { return ".NET welcomes " + input.ToString(); }');
 
-//Make method externaly visible this will be referenced in the render.js file
+//Make method externaly visible this will be referenced in the renderer.js file
 exports.sayHello = arg => {
 	hello('Electron', function (error, result) {
 		if (error) throw error;
