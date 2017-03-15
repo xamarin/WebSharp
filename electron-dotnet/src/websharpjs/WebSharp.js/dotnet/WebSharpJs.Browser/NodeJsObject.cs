dab69d55170207f904b2b27725bf0666edb73950
Kenneth Pouncey <kjpou@pt.lu>
2017-03-15 10:46:32 +0100

    Add NodeJsObject to serve as base class for NodeJs/Electron objects.

---
+++ electron-dotnet/src/websharpjs/WebSharp.js/dotnet/WebSharpJs.Browser/NodeJsObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSharpJs.Browser
+{
+    public class NodeJsObject : WebSharpObject
+    {
+        protected NodeJsObject() : base() { }
+
+        protected NodeJsObject(dynamic obj) : base((object)obj)
+        {
+            var dict = obj as IDictionary<string, object>;
+
+            // The key `websharp_id` represents a wrapped proxy object
+            if (dict != null && dict.ContainsKey("websharp_id"))
+            {
+                ScriptObjectProxy = new NodeObjectProxy(obj);
+            }
+        }
+
+        protected NodeJsObject(ScriptObjectProxy scriptObject)
+        {
+            ScriptObjectProxy = new NodeObjectProxy(scriptObject);
+        }
+
+        public static explicit operator NodeJsObject(ScriptObjectProxy sop)
+        {
+            return new NodeJsObject(sop);
+        }
+
+    }
+}
