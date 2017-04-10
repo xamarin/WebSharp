ca3149ce35422c5302210a213fafa18af20e246f
Kenneth Pouncey <kjpou@pt.lu>
2017-04-10 09:45:56 +0200

    Fix crash for mono 5.0.

--- electron-dotnet/src/mono/dictionary.cpp
+++ electron-dotnet/src/mono/dictionary.cpp
@@ -3,10 +3,19 @@
 void Dictionary::Add(MonoObject* _this, const char* name, MonoObject* value)
 {
     static MonoMethod* add;
-    
     if(!add) {
         add = mono_class_get_method_from_name(mono_object_get_class(_this), 
-            "System.Collections.Generic.IDictionary<string,object>.Add", -1);
+             "System.Collections.Generic.IDictionary<string,object>.Add", -1);
+
+        // This is to work around a crash due to updating to MONO 5.0.
+        // We still need the previous call to be backwards compatible.
+        if (add == NULL)
+        {
+
+            add = mono_class_get_method_from_name(mono_object_get_class(_this), 
+                "System.Collections.Generic.IDictionary<System.String,System.Object>.Add", -1);            
+        }
+
     }
 
     void* params[2];
