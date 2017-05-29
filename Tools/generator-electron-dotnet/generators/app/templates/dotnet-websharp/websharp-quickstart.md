56afffd67222f8487bd6f9a4ba9d8360b7860ada
Kenneth Pouncey <kjpou@pt.lu>
2017-05-29 15:56:15 +0200

    Add Electron DesktopCapturer support

--- Tools/generator-electron-dotnet/generators/app/templates/dotnet-websharp/websharp-quickstart.md
+++ Tools/generator-electron-dotnet/generators/app/templates/dotnet-websharp/websharp-quickstart.md
@@ -21,14 +21,14 @@ This folder contains all of the files necessary for your application.
 |--- package.json                     // Various project metadata
 |--- README.md
 |--- renderer.js                      // Required in index.html and executed in the renderer process for that window 
-|--- Main                             
-    |--- nuget.config                 // Configuring NuGet behavior
-    |--- packages.config              // Used to track installed packages
-    |--- MainWindow_macosx.csproj     // MacOSX project  
-    |--- MainWindow.cs                // Controls the applications main event lifecycle via managed code
-    |--- MainWindow.csproj            // Windows project          
 |--- src                              // sources
      |--- <%- name %>.js                    // javascript code implementation
+     |--- Main                             
+          |--- nuget.config                 // Configuring NuGet behavior
+          |--- packages.config              // Used to track installed packages
+          |--- MainWindow_macosx.csproj     // MacOSX project  
+          |--- MainWindow.cs                // Controls the applications main event lifecycle via managed code
+          |--- MainWindow.csproj            // Windows project          
      |--- <%- wsClassName %>
           |--- nuget.config           // Configuring NuGet behavior
           |--- packages.config        // Used to track installed packages
---
+++ electron-dotnet/src/websharpjs/WebSharp.js/dotnet/WebSharpJs.Electron/DesktopCapturer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using WebSharpJs.NodeJS;
+using WebSharpJs.Script;
+
+namespace WebSharpJs.Electron
+{
+    public class DesktopCapturer : NodeJsObject
+    {
+        protected override string ScriptProxy => @"desktopCapturer; ";
+        protected override string Requires => @"const {desktopCapturer} = require('electron');";
+
+        static DesktopCapturer proxy;
+
+        public static async Task<DesktopCapturer> Instance()
+        {
+            if (proxy == null)
+            {
+                proxy = new DesktopCapturer();
+                await proxy.Initialize();
+            }
+            return proxy;
+        }
+
+        protected DesktopCapturer() : base() { }
+
+        protected DesktopCapturer(object scriptObject) : base(scriptObject)
+        { }
+
+        public DesktopCapturer(ScriptObjectProxy scriptObject) : base(scriptObject)
+        { }
+
+        public static explicit operator DesktopCapturer(ScriptObjectProxy sop)
+        {
+            return new DesktopCapturer(sop);
+        }
+
+        public async Task<object> GetSources(DesktopCapturerOptions options, ScriptObjectCallback<Error, DesktopCapturerSource[]> callback)
+        {
+            return await Invoke<object>("getSources", options, callback);
+        }
+
+    }
+
+
+    [Flags]
+    public enum DesktopCapturerType
+    {
+        Screen,
+        Window
+    }
+
+    [ScriptableType]
+    public struct DesktopCapturerOptions
+    {
+        [ScriptableMember(ScriptAlias = "types")]
+        public string[] TypeDescriptions
+        {
+            get
+            {
+                if (Types == null || !Types.HasValue)
+                    return new string[0];
+
+                var properties = new List<string>();
+
+                if (Types.Value.HasFlag(DesktopCapturerType.Screen))
+                    properties.Add("screen");
+                if (Types.Value.HasFlag(DesktopCapturerType.Window))
+                    properties.Add("window");
+                return properties.ToArray();
+            }
+        
+        }
+        public DesktopCapturerType? Types { get; set; }
+        public Size ThumbnailSize { get; set; }
+    }
+
+}
