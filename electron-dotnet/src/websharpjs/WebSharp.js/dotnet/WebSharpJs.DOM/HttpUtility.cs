76234615931d0b625dd53136c194b2de0aae46fb
Kenneth Pouncey <kjpou@pt.lu>
2017-06-03 09:31:50 +0200

    Add HttpUtility to WebSharpJs.DOM.
    Provides methods for encoding and decoding HTML and URL strings.

---
+++ electron-dotnet/src/websharpjs/WebSharp.js/dotnet/WebSharpJs.DOM/HttpUtility.cs
@@ -0,0 +1,37 @@
+﻿namespace WebSharpJs.DOM
+{
+    using System.Net;
+
+    public static class HttpUtility
+    {
+        #region HtmlEncode and HtmlDecode methods
+
+        public static string HtmlEncode(string value)
+        {
+            return WebUtility.HtmlEncode(value);
+        }
+
+        public static string HtmlDecode(string value)
+        {
+            return WebUtility.HtmlDecode(value);
+        }
+
+        #endregion
+
+        #region UrlEncode and UrlDecode public methods
+
+        public static string UrlEncode(string value)
+        {
+            return WebUtility.UrlEncode(value);
+        }
+
+        public static string UrlDecode(string encodedValue)
+        {
+            return WebUtility.UrlDecode(encodedValue);
+        }
+
+        #endregion
+
+
+    }
+}
